40c486e036562cafa50e51b11b9198b6646f51eb
MarSamm <msamayoam@ufm.edu>
2014-12-09 16:27:20 -0600

    Ya con JavaScript, le agrege el basic CSS
    Ya casi todos tienen tags falta mara pero... yay

--- bw.js
+++ bw.js
@@ -1,16 +1,310 @@
+$(document).ready(function () {
+  $('button').click(function () {
+    $('button').addClass('bw');
+    $('button').removeClass('selected');
+    $(this).addClass('selected');
+  })
 
-var artTag;
+})
+
+//Visual Arts
+
+var visTag;
 
 if (document.querySelectorAll) {
-  artTag = document.querySelectorAll('.art');
+  visTag = document.querySelectorAll('.vis');
 }
 
 
 $(function(){
-  $('#Art').click(function (){
+  $('#VisA').click(function (){
+    $('.all').addClass('bw');
     console.log("here");
-    console.log(artTag);
-    $(artTag).removeClass('bw');
+    console.log(visTag);
+    $(visTag).removeClass('bw');
   });
 });
 
+//Performing Arts
+
+var perATag;
+
+if (document.querySelectorAll) {
+  perATag = document.querySelectorAll('.per');
+}
+
+
+$(function(){
+  $('#PerA').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(perATag);
+    $(perATag).removeClass('bw');
+  });
+});
+
+//Culinary Arts
+
+var culATag;
+
+if (document.querySelectorAll) {
+  culATag = document.querySelectorAll('.cul');
+}
+
+
+$(function(){
+  $('#CulA').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(culATag);
+    $(culATag).removeClass('bw');
+  });
+});
+
+
+//Music
+
+
+var musTag;
+
+if (document.querySelectorAll) {
+  musTag = document.querySelectorAll('.mus');
+}
+
+
+$(function(){
+  $('#Mus').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(musTag);
+    $(musTag).removeClass('bw');
+  });
+});
+
+
+//Humanities
+
+var humTag;
+
+if (document.querySelectorAll) {
+  humTag = document.querySelectorAll('.hum');
+}
+
+
+$(function(){
+  $('#Hum').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(humTag);
+    $(humTag).removeClass('bw');
+  });
+});
+
+//Programming
+
+var progTag;
+
+if (document.querySelectorAll) {
+  progTag = document.querySelectorAll('.prog');
+}
+
+
+$(function(){
+  $('#Prog').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(progTag);
+    $(progTag).removeClass('bw');
+  });
+});
+
+
+
+//Education
+
+
+var edTag;
+
+if (document.querySelectorAll) {
+  edTag = document.querySelectorAll('.ed');
+}
+
+
+$(function(){
+  $('#Ed').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(edTag);
+    $(edTag).removeClass('bw');
+  });
+});
+
+//Communication
+
+
+var comTag;
+
+if (document.querySelectorAll) {
+  comTag = document.querySelectorAll('.com');
+}
+
+
+$(function(){
+  $('#Com').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(comTag);
+    $(comTag).removeClass('bw');
+  });
+});
+
+//Entrepeneurship
+
+
+var entreTag;
+
+if (document.querySelectorAll) {
+  entreTag = document.querySelectorAll('.entre');
+}
+
+
+$(function(){
+  $('#Entre').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(entreTag);
+    $(entreTag).removeClass('bw');
+  });
+});
+
+// Economy and Politics
+
+
+var econTag;
+
+if (document.querySelectorAll) {
+  econPTag = document.querySelectorAll('.econ');
+}
+
+
+$(function(){
+  $('#Econ').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(econTag);
+    $(econTag).removeClass('bw');
+  });
+});
+
+//Politics
+
+var polTag;
+
+if (document.querySelectorAll) {
+  polTag = document.querySelectorAll('.pol');
+}
+
+
+$(function(){
+  $('#Pol').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(polTag);
+    $(polTag).removeClass('bw');
+  });
+});
+
+//Literature
+
+
+var litTag;
+
+if (document.querySelectorAll) {
+  litTag = document.querySelectorAll('.lit');
+}
+
+
+$(function(){
+  $('#Lit').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(litTag);
+    $(litTag).removeClass('bw');
+  });
+});
+
+//History
+
+
+var hisTag;
+
+if (document.querySelectorAll) {
+  hisTag = document.querySelectorAll('.his');
+}
+
+
+$(function(){
+  $('#His').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(hisTag);
+    $(hisTag).removeClass('bw');
+  });
+});
+
+//Philospphy
+
+
+var philTag;
+
+if (document.querySelectorAll) {
+  philTag = document.querySelectorAll('.phil');
+}
+
+
+$(function(){
+  $('#Phil').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(philTag);
+    $(philTag).removeClass('bw');
+  });
+});
+
+//Natural Sciences
+
+var natTag;
+
+if (document.querySelectorAll) {
+  natTag = document.querySelectorAll('.nat');
+}
+
+
+$(function(){
+  $('#Nat').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(natTag);
+    $(natTag).removeClass('bw');
+  });
+
+});
+
+//Hard Sciences
+
+
+var harTag;
+
+if (document.querySelectorAll) {
+  harTag = document.querySelectorAll('.har');
+}
+
+
+$(function(){
+  $('#Har').click(function (){
+    $('.all').addClass('bw');
+    console.log("here");
+    console.log(harTag);
+    $(harTag).removeClass('bw');
+  });
+});
