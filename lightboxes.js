ed8407544c4d86c5141de0d66501ac1323a7935b
nachogal <ignacio@juntostudio.com>
2014-12-10 17:22:07 -0600

    Lightboxes done, new css for lightboxes

--- lightboxes.js
+++ lightboxes.js
@@ -1,31 +1,42 @@
 
-var $overlay = $('<div id="overlay"></div');
+//Click capture event on an image
+//Show overlay
+//Update overlay with image
+//Add caption 
+
+//Add overlay with image and caption
+
+//When overlay is clicked hide the overlay
+
+var $overlay = $('<div id="overlay"></div>'); 
 var $image = $("<img>");
-var $caption = $("<p></p>");
+var $quote = $("<p></p>");
 
 $overlay.append($image);
 
-$overlay.append($caption);
+$overlay.append($quote);
 
 $("body").append($overlay);
 
-
 $("#flex a").click(function(event) {
-  event.preventDefault();
-  var imageLocation = $(this).attr("href");
-  
-  $image.attr("src",imageLocation);
-  
-  $overlay.show();
-  
-  var captionText = $(this).children("img").attr("alt");
-  $caption.text(captionText);
-  
-});
+	event.preventDefault();
+	var imageLocation = $(this).attr("href");
 
+	$image.attr("src", imageLocation);
 
+	$overlay.show(); 
+
+	var quoteText = $(this).children("img").attr("alt");
+	$quote.text(quoteText);
+
+});	
 
 $overlay.click(function(){
-  $overlay.hide();
-  
+	$overlay.hide();
 });
+
+
+
+
+
+
